17a8bed95386f9f03b47497bfcd9bdc880dac34c
Nguyen-Ha-Anh <anhnhf8@fullstack.edu.vn>
2026-01-05 11:46:30 +0700

    fixed

--- Homework/D37-Shadcn/src/ui/Download/Download.jsx
+++ Homework/D37-Shadcn/src/ui/Download/Download.jsx
@@ -4,7 +4,6 @@ export default function Download() {
   return (
     <div className="container py-8 mx-auto">
       <div className="relative bg-[#F2F2F2] rounded-2xl h-[611px] overflow-hidden">
-
         {/* left */}
         <div className="absolute left-0 bottom-0 z-30">
           <img
@@ -19,34 +18,43 @@ export default function Download() {
           />
         </div>
         {/* right */}
-        <div className="relative z-10 flex h-full items-center left-[575px]">
-          <div className="">
-            <h1 className="flex font-bold text-[#0B0D26] mb-4">
-               <img src="/images/logo.png" className="
+        <div className="relative z-10 flex h-full items-center">
+          <div className="relative left-[800px]">
+            <h1 className="flex font-bold text-[#0B0D26] mb-6">
+              <img
+                src="/images/logo.png"
+                className="
                relative
                w-[268px]
-               h-[66px]"/>
-               <p className="!text-7xl w-[450px]">ing is more</p>
+               h-[66px]"
+              />
+              <p className="!text-7xl w-[450px] font-bold">ing is more</p>
             </h1>
 
-            <div className="flex w-[878px] h-[90px] bg-[#03081F] left-[665px] rounded-full px-6 py-3 mb-6">
-              <span className="text-orange-400 font-semibold">
-                Personalised
-              </span>
-              <span className="text-white"> & Instant</span>
+            <div className="relative flex w-[878px] h-[90px] bg-[#03081F] right-[220px] rounded-full px-6 py-3 mb-6">
+              <div className="absolute h-[81px] text-6xl right-[40px]">
+                <span className="text-orange-400 font-medium underline decoration-2">
+                  Personalised
+                </span>
+                <span className="text-white"> & Instant</span>
+              </div>
             </div>
 
-            <p className="text-gray-600 mb-6">
+            <p className="relative w-[488px] h-[36px] text-2xl left-[120px]">
               Download the Order.uk app for faster ordering
             </p>
 
-            <div className="flex gap-4">
-              <img src="/images/appstore.png" className="h-12" />
-              <img src="/images/googleplay.png" className="h-12" />
+            <div className="flex relative left-[120px] items-center">
+              <img
+                src="/images/downappstore.webp"
+                alt="App Store"
+                className="w-auto h-[100px]"
+              />
+              <img src="/images/downgoogplay.png" alt="Google Play"
+              className="relative w-[220px] h-[135px] -left-[25px] object-cover z-99" />
             </div>
           </div>
         </div>
-
       </div>
     </div>
   );
